48903d30be4612090916715ff9bad07cf9b10718
Linus Westling <westling.linus@gmail.com>
2026-02-02 12:33:09 +0100

    Created dependencies and project structure + reasoning

---
+++ src/main/java/com/example/payment/PaymentAPI.java
@@ -0,0 +1,6 @@
+package com.example.payment;
+
+// Server requests need to be isolated given the number of things that can go wrong. Best structure, in my opinion, is obtained by doing this in a separate class.
+//      Can be made ascyncronus later to handle several payments simultaneously
+public class PaymentAPI {
+}
