c0d9ec4602960501d42f99dfcc7e15d37099d87c
Linus Westling <westling.linus@gmail.com>
2026-02-05 13:07:16 +0100

    Fundamental structure to begin writing tests

--- src/test/java/com/example/ShoppingCartTest.java
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -1,10 +1,25 @@
 package com.example;
 
+import com.example.shop.Product;
+import com.example.shop.ShoppingCart;
+import org.junit.jupiter.api.BeforeEach;
+
 public class ShoppingCartTest {
-    // Fields??
+
+    private ShoppingCart cart;
+    private Product orange;
+    private Product pineapple;
+
+    @BeforeEach
+    void setup() {
+        cart = new ShoppingCart();
+        orange = new Product("Orange", 8.0);
+        pineapple = new Product("Pineapple", 15.0);
+    }
 
 
     // Add products
+
 
 
     // Remove products
